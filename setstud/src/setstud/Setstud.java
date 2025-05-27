7d7d6030b0d1b88fa4320088b7645fd08e1efdaa
net671 <ulyssesmelendres1@gmail.com>
2025-05-26 15:42:46 -1200

    Add files via upload

---
+++ setstud/src/setstud/Setstud.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package setstud;
+
+/**
+ *
+ * @author USER
+ */
+public class Setstud {
+     
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       newstud open = new newstud();
+        open.setVisible(true);
+        
+    }
+    
+}
